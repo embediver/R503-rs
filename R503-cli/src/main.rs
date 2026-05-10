63f65f9a36d6d96a45ea75e19d2c6bcddb9e3aca
Jasmin Feldmann <github.jasmin@feldmann.pro>
2026-05-10 18:47:43 +0200

    add clap cli derive

--- R503-cli/src/main.rs
+++ R503-cli/src/main.rs
@@ -1,5 +1,21 @@
 use R503::R503;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(version, long_about = None)]
+/// Command line interface for R503 module. This is a simple example of how to use the R503 struct.
+struct Args {
+    /// Serial port to which the module is connected. For example: `COM3` on Windows or `/dev/ttyUSB0` on Linux.
+    #[arg()]
+    port: String,
+    /// Password for the R503 module. This is optional. Defaults to `0x00000000`.
+    #[arg(short, long)]
+    pwd: Option<u32>,
+    /// Address of the R503 module. This is optional. Defaults to `0xFFFFFFFF`.
+    #[arg(short, long)]
+    addr: Option<u32>,
+}
 
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
